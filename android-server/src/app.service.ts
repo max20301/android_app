b0b595000038350db9f5d41f2efb904fe474f9ab
max20301 <b09901052@ntu.edu.tw>
2021-05-18 18:24:13 +0800

    return json

--- android-server/src/app.service.ts
+++ android-server/src/app.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@nestjs/common';
 
+declare namespace postdata {
+  export interface Post {
+    author: string;
+    photo: string;
+    timestamp: number;
+    content: string;
+    image_content: string;
+  }
+
+  export interface Data {
+    posts: Post[];
+  }
+
+  export interface RootObject {
+    status: number;
+    data: Data;
+  }
+}
+
+class jsonData implements postdata.RootObject {
+  status: number;
+  data: postdata.Data;
+
+  constructor() {
+    this.status = 0;
+    this.data = {
+      posts: [],
+    };
+  }
+
+  addpost(
+    author: string,
+    photo: string,
+    timestamp: number,
+    content: string,
+    image_content: string,
+  ): void {
+    this.data.posts.push({
+      author: author,
+      photo: photo,
+      timestamp: timestamp,
+      content: content,
+      image_content: image_content,
+    });
+  }
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const jsonObject = new jsonData();
+    jsonObject.addpost(
+      'Jim',
+      'https://imgur.com/dXL2o6k',
+      1619523061,
+      '最近澱粉減量改吃蛋白質，結果膽固醇就過高了耶\uD83D\uDE11\uD83D\uDE11\uD83D\uDE11\uD83D\uDE44\n人生好難...\uD83C\uDF43',
+      'https://imgur.com/dBX3PCR',
+    );
+    return JSON.stringify(jsonObject);
   }
 }
